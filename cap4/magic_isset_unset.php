6ab9c1dacef5eff287a2d379ad919d8f6d3485f2
Wallace <wllcblm@gmail.com>
2019-11-28 00:05:45 -0300

    Alterando comportamento métodos mágicos __isset e __unset

---
+++ cap4/magic_isset_unset.php
@@ -0,0 +1,52 @@
+<?php
+
+class Titulo
+{
+    private $data;
+
+    public function __set($propriedade, $valor)
+    {
+        $this->data[$propriedade] = $valor;
+    }
+
+    public function __get($propriedade)
+    {
+        return $this->data[$propriedade];
+    }
+
+    public function __isset($propriedade)
+    {
+        return isset($this->data[$propriedade]);
+    }
+
+    public function __unset($propriedade)
+    {
+        unset($this->data[$propriedade]);
+    }
+}
+
+$titulo = new Titulo;
+$titulo->valor = 12345;
+print 'O valor é: ' . number_format($titulo->valor, 2, ',', '.');
+
+print '<br/><br/>Verificando atribuição à propriedade... <br/><br/>';
+
+if (isset($titulo->valor)) {
+    print "Valor definido<br/>\n";
+} else {
+    print "Valor não definido<br/>\n";
+}
+
+print '<br/><br/>Resetando valor da propriedade... <br/><br/>';
+
+unset($titulo->valor);
+
+print '<br>';
+
+if (isset($titulo->valor)) {
+    print "Valor definido<br/>\n";
+} else {
+    print "Valor não definido<br/>\n";
+}
+
+print 'O valor é: ' . number_format($titulo->valor, 2, ',', '.');
